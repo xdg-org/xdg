88b3246e9a8fadbd629db4a004cdbc6404f78724
waqar-ukaea <waqar.butt@ukaea.uk>
2025-04-11 11:04:31 +0100

    Moved GPRTRaytracer method declarations into XDG namespace

--- src/gprt/ray_tracer.cpp
+++ src/gprt/ray_tracer.cpp
@@ -3,4 +3,21 @@
 
 namespace xdg {
 
+GPRTRayTracer::GPRTRayTracer()
+{
+  context_ = gprtContextCreate();
+}
+
+GPRTRayTracer::~GPRTRayTracer()
+{
+  gprtContextDestroy(context_);
+}
+
+// void GPRTRayTracer::add_module(GPRTProgram device_code, std::string name)
+// {
+//   GPRTModule module = gprtModuleCreate(context_, device_code);
+//   device_codes_[name] = module;
+// }
+
+
 } // namespace xdg
