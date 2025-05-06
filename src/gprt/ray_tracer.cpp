d4e37fe4dda38e2e93ce326b65ed4118c11c9801
waqar-ukaea <waqar.butt@ukaea.uk>
2025-05-06 09:58:49 +0100

    Started on implementation of GPRT::ray_fire()
    Restructured class heirachy for GPRTRaytracer
    
    Added file stubs
    
    Fixed #includes for the flt and dbl GPRTRayTracers
    
    Added compilation of shaders for GPRTRayTracer<float>
    
    Added compilation of shaders for GPRTRayTracer<double>
    
    Continuing to rewrite double precision plucker intersection shader
    
    Wrote simple shaders for 1DRayGen
    
    Modified CMakeLists.txt to set up a GPRT based particle-sim tool
    
    Restructured GPRTRayTracer class

--- src/gprt/ray_tracer.cpp
+++ src/gprt/ray_tracer.cpp
@@ -1,69 +1,96 @@
-#include "xdg/gprt/ray_tracer.h"
+// #include "xdg/gprt/ray_tracer.h"
 
 
-namespace xdg {
+// namespace xdg {
 
-GPRTRayTracer::GPRTRayTracer()
-{
-  context_ = gprtContextCreate();
-}
+// GPRTRayTracer::GPRTRayTracer()
+// {
+//   context_ = gprtContextCreate();
+// }
 
-GPRTRayTracer::~GPRTRayTracer()
-{
-  gprtContextDestroy(context_);
-}
+// GPRTRayTracer::~GPRTRayTracer()
+// {
+//   gprtContextDestroy(context_);
+// }
 
-// Ray tracer interface stub methods to be implemented
-void GPRTRayTracer::init() {
-  // TODO: Init GPRT context and modules
-}
+// // Ray tracer interface stub methods to be implemented
+// void GPRTRayTracer::init() {
+//   // TODO: Init GPRT context and modules
+// }
 
-TreeID GPRTRayTracer::register_volume(const std::shared_ptr<MeshManager> mesh_manager, MeshID volume) {
-  // TODO: Register GPRT geometry
-  return {}; // placeholder
-}
+// TreeID GPRTRayTracer::register_volume(const std::shared_ptr<MeshManager> mesh_manager, MeshID volume) {
+//   // TODO: Register GPRT geometry
+//   return {}; // placeholder
+// }
 
-bool GPRTRayTracer::point_in_volume(TreeID scene,
-                                     const Position& point,
-                                     const Direction* direction,
-                                     const std::vector<MeshID>* exclude_primitives) const {
-  // TODO: Point containment logic
-  return false;
-}
+// bool GPRTRayTracer::point_in_volume(TreeID scene,
+//                                      const Position& point,
+//                                      const Direction* direction,
+//                                      const std::vector<MeshID>* exclude_primitives) const {
+//   // TODO: Point containment logic
+//   return false;
+// }
+  
 
-std::pair<double, MeshID> GPRTRayTracer::ray_fire(TreeID scene,
-                                                  const Position& origin,
-                                                  const Direction& direction,
-                                                  const double dist_limit,
-                                                  HitOrientation orientation,
-                                                  std::vector<MeshID>* const exclude_primitives) {
-  // TODO: Ray cast logic
-  return {0.0, 0};
-}
+//   void GPRTRayTracer::ray_fire(TreeID scene,
+//                              const std::vector<Position>& origins,
+//                              const std::vector<Direction>& directions,
+//                              double dist_limit,
+//                              HitOrientation orientation,
+//                              const std::vector<MeshID>* exclude_primitives) {
+//     // Create buffer to store ray data
+//     VkBuffer rayBuffer;
+//     VkDeviceMemory rayBufferMemory;
+//     // Allocate and fill rayBuffer with origins and directions...
+//     std::vector<GPRTBufferOf<float3>> particleOriginBuffer;
+//     std::vector<GPRTBufferOf<float3>> particleDirectionBuffer;
+//     for (size_t i = 0; i < origins.size(); ++i) {
+//         particleOriginBuffer.push_back(gprtDeviceBufferCreate<float3>(context_, origins[i]));
+//         particleDirectionBuffer.push_back(gprtDeviceBufferCreate<float3>(context_, directions[i]));
+//     }
+//     // Need to think about handling single vs double precision floating points
 
-void GPRTRayTracer::closest(TreeID scene,
-                            const Position& origin,
-                            double& dist,
-                            MeshID& triangle) {
-  // TODO: Closest hit logic with triangle
-  dist = -1.0;
-  triangle = -1;
-}
+//     RayFirePushConstants pc;
+//     pc.dist_limit = dist_limit;
+//     pc.orientation = orientation;
+//     pc.num_rays = origins.size();
 
-void GPRTRayTracer::closest(TreeID scene,
-                            const Position& origin,
-                            double& dist) {
-  // TODO: Closest hit logic
-  dist = -1.0;
-}
+//     // Launch the ray generation shader with push constants and buffer bindings
+//     gprtRayGenLaunch1D(context_, rayGen, origins.size(), rayBuffer, pc);
 
-bool GPRTRayTracer::occluded(TreeID scene,
-                             const Position& origin,
-                             const Direction& direction,
-                             double& dist) const {
-  // TODO: Occlusion logic
-  dist = -1.0;
-  return false;
-}
+//     // This will launch the rays and run our shaders in the ray tracing pipeline
+//     // miss shader returns dist = 0.0 and elementID = -1
+//     // closest hit shader returns dist = distance to hit and elementID = triangle ID
+// }
+//   */
+                                                  
 
-} // namespace xdg
+//   return {0.0, 0};
+// }
+
+// void GPRTRayTracer::closest(TreeID scene,
+//                             const Position& origin,
+//                             double& dist,
+//                             MeshID& triangle) {
+//   // TODO: Closest hit logic with triangle
+//   dist = -1.0;
+//   triangle = -1;
+// }
+
+// void GPRTRayTracer::closest(TreeID scene,
+//                             const Position& origin,
+//                             double& dist) {
+//   // TODO: Closest hit logic
+//   dist = -1.0;
+// }
+
+// bool GPRTRayTracer::occluded(TreeID scene,
+//                              const Position& origin,
+//                              const Direction& direction,
+//                              double& dist) const {
+//   // TODO: Occlusion logic
+//   dist = -1.0;
+//   return false;
+// }
+
+// } // namespace xdg
