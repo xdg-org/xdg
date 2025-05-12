150325a9b7530fc884591daa5f1966192a578123
Waqar-ukaea <Waqar-ukaea@users.noreply.github.com>
2025-05-12 15:59:21 +0000

    deploy: c97fa3bb01d978ad48cac83c2cddd820c2faf32b

--- search/all_6.js
+++ search/all_6.js
@@ -1,8 +1,5 @@
 var searchData=
 [
   ['geometryuserdata_0',['GeometryUserData',['../structxdg_1_1GeometryUserData.html',1,'xdg']]],
-  ['get_5fmb_5fcoords_1',['get_mb_coords',['../classMBDirectAccess.html#af6f3e85d6ef6faf5696f9ed8b00ed0f8',1,'MBDirectAccess']]],
-  ['gprtaccel_2',['GPRTAccel',['../structxdg_1_1GPRTAccel.html',1,'xdg']]],
-  ['gprtcontext_3',['GPRTContext',['../structxdg_1_1GPRTContext.html',1,'xdg']]],
-  ['gprtgeom_4',['GPRTGeom',['../structxdg_1_1GPRTGeom.html',1,'xdg']]]
+  ['get_5fmb_5fcoords_1',['get_mb_coords',['../classMBDirectAccess.html#af6f3e85d6ef6faf5696f9ed8b00ed0f8',1,'MBDirectAccess']]]
 ];
--- search/classes_3.js
+++ search/classes_3.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['geometryuserdata_0',['GeometryUserData',['../structxdg_1_1GeometryUserData.html',1,'xdg']]],
-  ['gprtaccel_1',['GPRTAccel',['../structxdg_1_1GPRTAccel.html',1,'xdg']]],
-  ['gprtcontext_2',['GPRTContext',['../structxdg_1_1GPRTContext.html',1,'xdg']]],
-  ['gprtgeom_3',['GPRTGeom',['../structxdg_1_1GPRTGeom.html',1,'xdg']]]
+  ['geometryuserdata_0',['GeometryUserData',['../structxdg_1_1GeometryUserData.html',1,'xdg']]]
 ];
