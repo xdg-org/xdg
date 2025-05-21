de8edf6144931e8bf7c2f0ec3d7f6e2a451635c9
gonuke <gonuke@users.noreply.github.com>
2025-05-21 15:08:14 +0000

    deploy: fe3dd89a5eafb03efa93e14fb3a54ca4de0e5b38

--- search/all_c.js
+++ search/all_c.js
@@ -3,7 +3,6 @@ var searchData=
   ['prim_5fref_5fbuffer_0',['prim_ref_buffer',['../structxdg_1_1GeometryUserData.html#ac70dd33b3aa4a3d571b78c7ddc8aaa2f',1,'xdg::GeometryUserData']]],
   ['primitive_5fref_1',['primitive_ref',['../structxdg_1_1RTCDHit.html#ac2a73c61fde663e67dfc019c7b455112',1,'xdg::RTCDHit::primitive_ref'],['../structxdg_1_1RTCDPointQuery.html#a2ccd9302cab7d372b5ae305634598dee',1,'xdg::RTCDPointQuery::primitive_ref']]],
   ['primitiveref_2',['PrimitiveRef',['../structxdg_1_1PrimitiveRef.html',1,'xdg']]],
-  ['progressbar_3',['ProgressBar',['../classProgressBar.html',1,'']]],
-  ['property_4',['Property',['../structxdg_1_1Property.html',1,'xdg']]],
-  ['propertydata_5',['PropertyData',['../structxdg_1_1PropertyData.html',1,'xdg']]]
+  ['property_3',['Property',['../structxdg_1_1Property.html',1,'xdg']]],
+  ['propertydata_4',['PropertyData',['../structxdg_1_1PropertyData.html',1,'xdg']]]
 ];
--- search/classes_6.js
+++ search/classes_6.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['primitiveref_0',['PrimitiveRef',['../structxdg_1_1PrimitiveRef.html',1,'xdg']]],
-  ['progressbar_1',['ProgressBar',['../classProgressBar.html',1,'']]],
-  ['property_2',['Property',['../structxdg_1_1Property.html',1,'xdg']]],
-  ['propertydata_3',['PropertyData',['../structxdg_1_1PropertyData.html',1,'xdg']]]
+  ['property_1',['Property',['../structxdg_1_1Property.html',1,'xdg']]],
+  ['propertydata_2',['PropertyData',['../structxdg_1_1PropertyData.html',1,'xdg']]]
 ];
