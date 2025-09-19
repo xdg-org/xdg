c556f76f487ebcd96fe66c9d36199d8ce0bf1fdf
waqar-ukaea <waqar.butt@ukaea.uk>
2025-09-19 11:31:02 +0100

    Removed redundant constants from sharedCode.h header

--- include/xdg/gprt/sharedCode.h
+++ include/xdg/gprt/sharedCode.h
@@ -30,12 +30,6 @@ using double4 = math::double4;
 
 #endif // __SLANG__
 
-
-#define AA 3 // used for antialiasing
-#define EPSILON 2.2204460492503130808472633361816E-16
-// #define FLT_EPSILON	1.19209290e-7F
-// #define DBL_EPSILON	2.2204460492503131e-16
-
 enum PointInVolume : int { Outside = 0, Inside = 1 };
 
 struct GPRTPrimitiveRef
