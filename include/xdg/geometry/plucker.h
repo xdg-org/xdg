652eec9324b79288c8fd2e6ae1cb219af04c1a46
waqar-ukaea <waqar.butt@ukaea.uk>
2025-10-03 15:52:53 +0100

    Attempting to add slang/c++ agnostic wrapper around plucker intersection tests

--- include/xdg/geometry/plucker.h
+++ include/xdg/geometry/plucker.h
@@ -2,6 +2,7 @@
 #define _XDG_PLUCKER_H
 
 #include "xdg/vec3da.h"
+#include "xdg/geometry/dp_math.h"
 
 namespace xdg {
 
@@ -20,10 +21,15 @@ namespace xdg {
  * (normal pointing out of the plane). This ordering is based on the reference:
  * https://doi.org/10.1002/cnm.1237
  */
-bool plucker_ray_tri_intersect(const std::array<Position, 3> vertices,
-                               const Position& origin,
-                               const Direction& direction,
-                               double& dist_out,
+
+struct PluckerIntersectionResult {
+  bool hit = false;    // Whether an intersection occurred
+  double t = 0.0;      // Distance along the ray to the intersection point
+};
+
+PluckerIntersectionResult plucker_ray_tri_intersect(const std::array<dp::vec3, 3> vertices,
+                               const dp::vec3& origin,
+                               const dp::vec3& direction,
                                const double nonneg_ray_len = INFTY,
                                const double* neg_ray_len = nullptr,
                                const int* orientation = nullptr);
