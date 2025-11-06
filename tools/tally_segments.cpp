1f0d7e814e5582876b84f19d458fb9dfaba00ea9
Patrick Shriwise <pshriwise@gmail.com>
2025-11-06 07:43:10 -0600

    Consistent indentation in main

--- tools/tally_segments.cpp
+++ tools/tally_segments.cpp
@@ -15,39 +15,39 @@ using namespace xdg;
 
 int main(int argc, char** argv) {
 
-// argument parsing
-argparse::ArgumentParser args("XDG Mesh Tally Simulator", "1.0", argparse::default_arguments::help);
+  // argument parsing
+  argparse::ArgumentParser args("XDG Mesh Tally Simulator", "1.0", argparse::default_arguments::help);
 
-args.add_argument("filename")
-    .help("Path to the input file");
+  args.add_argument("filename")
+      .help("Path to the input file");
 
-args.add_argument("-l", "--library")
-    .help("Mesh library to use. One of (MOAB, LIBMESH)")
-    .default_value("MOAB");
+  args.add_argument("-l", "--library")
+      .help("Mesh library to use. One of (MOAB, LIBMESH)")
+      .default_value("MOAB");
 
-args.add_argument("-n", "--num-tracks")
-    .help("Number of tracks to simulate")
-    .default_value(1000)
-    .scan<'i', int>();
+  args.add_argument("-n", "--num-tracks")
+      .help("Number of tracks to simulate")
+      .default_value(1000)
+      .scan<'i', int>();
 
-args.add_argument("-t", "--threads")
-    .help("Number of threads to use")
-    .default_value(-1)
-    .scan<'i', int>();
+  args.add_argument("-t", "--threads")
+      .help("Number of threads to use")
+      .default_value(-1)
+      .scan<'i', int>();
 
-args.add_argument("-v", "--verbose")
-    .default_value(false)
-    .implicit_value(true)
-    .help("Enable verbose output");
+  args.add_argument("-v", "--verbose")
+      .default_value(false)
+      .implicit_value(true)
+      .help("Enable verbose output");
 
-args.add_argument("-q", "--quiet")
-    .default_value(false)
-    .implicit_value(true)
-    .help("Minimize all output (for performance testing)");
+  args.add_argument("-q", "--quiet")
+      .default_value(false)
+      .implicit_value(true)
+      .help("Minimize all output (for performance testing)");
 
-args.add_argument("-c", "--check-tracks")
-    .help("Verify that track lengths always match the sum of segments")
-    .flag();
+  args.add_argument("-c", "--check-tracks")
+      .help("Verify that track lengths always match the sum of segments")
+      .flag();
 
   try {
     args.parse_args(argc, argv);
@@ -58,42 +58,42 @@ args.add_argument("-c", "--check-tracks")
     exit(0);
   }
 
-// Problem Setup
-srand48(42);
+  // Problem Setup
+  srand48(42);
 
-// create a mesh manager
-std::shared_ptr<XDG> xdg {nullptr};
-if (args.get<std::string>("--library") == "MOAB")
-  xdg = XDG::create(MeshLibrary::MOAB);
-else if (args.get<std::string>("--library") == "LIBMESH")
-  xdg = XDG::create(MeshLibrary::LIBMESH);
-else
-  fatal_error("Invalid mesh library {} specified", args.get<std::string>("--library"));
+  // create a mesh manager
+  std::shared_ptr<XDG> xdg {nullptr};
+  if (args.get<std::string>("--library") == "MOAB")
+    xdg = XDG::create(MeshLibrary::MOAB);
+  else if (args.get<std::string>("--library") == "LIBMESH")
+    xdg = XDG::create(MeshLibrary::LIBMESH);
+  else
+    fatal_error("Invalid mesh library {} specified", args.get<std::string>("--library"));
 
-const auto& mm = xdg->mesh_manager();
-mm->load_file(args.get<std::string>("filename"));
-mm->init();
-xdg->prepare_raytracer();
+  const auto& mm = xdg->mesh_manager();
+  mm->load_file(args.get<std::string>("filename"));
+  mm->init();
+  xdg->prepare_raytracer();
 
-// get the bounding box of the mesh
-BoundingBox bbox = xdg->mesh_manager()->global_bounding_box();
-std::cout << fmt::format("Mesh Bounding Box: {}", bbox) << std::endl;
+  // get the bounding box of the mesh
+  BoundingBox bbox = xdg->mesh_manager()->global_bounding_box();
+  std::cout << fmt::format("Mesh Bounding Box: {}", bbox) << std::endl;
 
-bool check_tracks = args.get<bool>("--check-tracks");
+  bool check_tracks = args.get<bool>("--check-tracks");
 
-size_t n_tracks = args.get<int>("--num-tracks");
+  size_t n_tracks = args.get<int>("--num-tracks");
 
-TallyContext tally_context;
-tally_context.xdg_ = xdg;
-int n_threads = args.get<int>("--threads");
-if (n_threads >= 1) tally_context.n_threads_ = n_threads;
-tally_context.n_tracks_ = args.get<int>("--num-tracks");
-tally_context.check_tracks_ = args.get<bool>("--check-tracks");
-tally_context.verbose_ = args.get<bool>("--verbose");
-tally_context.quiet_ = args.get<bool>("--quiet");
+  TallyContext tally_context;
+  tally_context.xdg_ = xdg;
+  int n_threads = args.get<int>("--threads");
+  if (n_threads >= 1) tally_context.n_threads_ = n_threads;
+  tally_context.n_tracks_ = args.get<int>("--num-tracks");
+  tally_context.check_tracks_ = args.get<bool>("--check-tracks");
+  tally_context.verbose_ = args.get<bool>("--verbose");
+  tally_context.quiet_ = args.get<bool>("--quiet");
 
-tally_segments(tally_context);
+  tally_segments(tally_context);
 
-return 0;
+  return 0;
 
 }
--- tools/walk_elements.cpp
+++ tools/walk_elements.cpp
@@ -15,39 +15,39 @@ using namespace xdg;
 
 int main(int argc, char** argv) {
 
-// argument parsing
-argparse::ArgumentParser args("XDG Volumetric Element Tracking Simulator", "1.0", argparse::default_arguments::help);
+  // argument parsing
+  argparse::ArgumentParser args("XDG Volumetric Element Tracking Simulator", "1.0", argparse::default_arguments::help);
 
-args.add_argument("filename")
-    .help("Path to the input file");
+  args.add_argument("filename")
+      .help("Path to the input file");
 
-args.add_argument("-l", "--library")
-    .help("Mesh library to use. One of (MOAB, LIBMESH)")
-    .default_value("MOAB");
+  args.add_argument("-l", "--library")
+      .help("Mesh library to use. One of (MOAB, LIBMESH)")
+      .default_value("MOAB");
 
-args.add_argument("-n", "--num-particles")
-    .help("Number of particles to simulate")
-    .default_value(1000)
-    .scan<'i', int>();
+  args.add_argument("-n", "--num-particles")
+      .help("Number of particles to simulate")
+      .default_value(1000)
+      .scan<'i', int>();
 
-args.add_argument("-t", "--threads")
-    .help("Number of threads to use")
-    .default_value(-1)
-    .scan<'i', int>();
+  args.add_argument("-t", "--threads")
+      .help("Number of threads to use")
+      .default_value(-1)
+      .scan<'i', int>();
 
-args.add_argument("-v", "--verbose")
-    .default_value(false)
-    .implicit_value(true)
-    .help("Enable verbose output of particle events");
+  args.add_argument("-v", "--verbose")
+      .default_value(false)
+      .implicit_value(true)
+      .help("Enable verbose output of particle events");
 
-args.add_argument("-q", "--quiet")
-    .default_value(false)
-    .implicit_value(true)
-    .help("Minimize all output (for performance testing)");
+  args.add_argument("-q", "--quiet")
+      .default_value(false)
+      .implicit_value(true)
+      .help("Minimize all output (for performance testing)");
 
-args.add_argument("-m", "--mfp")
-    .default_value(1.0)
-    .help("Mean free path of the particles").scan<'g', double>();
+  args.add_argument("-m", "--mfp")
+      .default_value(1.0)
+      .help("Mean free path of the particles").scan<'g', double>();
 
   try {
     args.parse_args(argc, argv);
@@ -58,35 +58,35 @@ args.add_argument("-m", "--mfp")
     exit(0);
   }
 
-// Problem Setup
-srand48(42);
+  // Problem Setup
+  srand48(42);
 
-// create a mesh manager
-std::shared_ptr<XDG> xdg {nullptr};
-if (args.get<std::string>("--library") == "MOAB")
-  xdg = XDG::create(MeshLibrary::MOAB);
-else if (args.get<std::string>("--library") == "LIBMESH")
-  xdg = XDG::create(MeshLibrary::LIBMESH);
-else
-  fatal_error("Invalid mesh library {} specified", args.get<std::string>("--library"));
+  // create a mesh manager
+  std::shared_ptr<XDG> xdg {nullptr};
+  if (args.get<std::string>("--library") == "MOAB")
+    xdg = XDG::create(MeshLibrary::MOAB);
+  else if (args.get<std::string>("--library") == "LIBMESH")
+    xdg = XDG::create(MeshLibrary::LIBMESH);
+  else
+    fatal_error("Invalid mesh library {} specified", args.get<std::string>("--library"));
 
-const auto& mm = xdg->mesh_manager();
-mm->load_file(args.get<std::string>("filename"));
-mm->init();
-mm->parse_metadata();
-xdg->prepare_raytracer();
+  const auto& mm = xdg->mesh_manager();
+  mm->load_file(args.get<std::string>("filename"));
+  mm->init();
+  mm->parse_metadata();
+  xdg->prepare_raytracer();
 
-WalkElementsContext walkelementscontext;
+  WalkElementsContext walkelementscontext;
 
-walkelementscontext.xdg_ = xdg;
-int n_threads = args.get<int>("--threads");
-if (n_threads >= 1) walkelementscontext.n_threads_ = n_threads;
-walkelementscontext.n_particles_ = args.get<int>("--num-particles");
-walkelementscontext.mean_free_path_ = args.get<double>("--mfp");
-walkelementscontext.verbose_ = args.get<bool>("--verbose");
-walkelementscontext.quiet_ = args.get<bool>("--quiet");
+  walkelementscontext.xdg_ = xdg;
+  int n_threads = args.get<int>("--threads");
+  if (n_threads >= 1) walkelementscontext.n_threads_ = n_threads;
+  walkelementscontext.n_particles_ = args.get<int>("--num-particles");
+  walkelementscontext.mean_free_path_ = args.get<double>("--mfp");
+  walkelementscontext.verbose_ = args.get<bool>("--verbose");
+  walkelementscontext.quiet_ = args.get<bool>("--quiet");
 
-walk_elements(walkelementscontext);
+  walk_elements(walkelementscontext);
 
-return 0;
+  return 0;
 }
