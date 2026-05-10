d092b43d86b347d21bc1c5eb67c5676a4f49bc58
magnoxemo <walid.ahammed01@gmail.com>
2026-05-09 20:04:12 -0500

    fix cmake build file to allow CI to compile this example

--- examples/ray_segments.cpp
+++ examples/ray_segments.cpp
@@ -9,13 +9,14 @@
 
 int main(int argc, char* argv[])
 {
-
+    // Not sure if this is the best way but based on CI matrix in the event
+    // where MOAB and LibMesh both defined we are gonna end up using MOAB mesh manager
 #if defined(XDG_ENABLE_MOAB)
     auto mesh_manager = std::make_shared<xdg::MOABMeshManager>();
-    mesh_manager->load_file("cube.h5m");
+    mesh_manager->load_file("tests/cube.h5m"); // using existing test files
 #elif defined(XDG_ENABLE_LIBMESH)
     auto mesh_manager = std::make_shared<xdg::LibMeshManager>();
-    mesh_manager->load_file("brick.exo");
+    mesh_manager->load_file("tests/brick.exo"); // using existing test files
 #endif
 
     mesh_manager->init();
