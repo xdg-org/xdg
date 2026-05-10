44a254a43824bf468c761e88485bc25cd77d09d7
magnoxemo <walid.ahammed01@gmail.com>
2026-05-09 20:18:24 -0500

    fix CMake

--- examples/ray_segments.cpp
+++ examples/ray_segments.cpp
@@ -13,10 +13,10 @@ int main(int argc, char* argv[])
     // where MOAB and LibMesh both defined we are gonna end up using MOAB mesh manager
 #if defined(XDG_ENABLE_MOAB)
     auto mesh_manager = std::make_shared<xdg::MOABMeshManager>();
-    mesh_manager->load_file("tests/cube.h5m"); // using existing test files
+    mesh_manager->load_file("cube.h5m");
 #elif defined(XDG_ENABLE_LIBMESH)
     auto mesh_manager = std::make_shared<xdg::LibMeshManager>();
-    mesh_manager->load_file("tests/brick.exo"); // using existing test files
+    mesh_manager->load_file("brick.exo");
 #endif
 
     mesh_manager->init();
